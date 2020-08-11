367dabde57ef58b73939c02d762958069bc34909
kwon <sjbby100@naver.com>
2020-08-11 16:47:44 +0900

    complete hooks tutorial

---
+++ src/components/SongList.jsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+import { v4 as uuid } from "uuid";
+import NewSongForm from "./NewSongForm";
+const SongList = () => {
+  const [songs, setSongs] = useState([
+    { title: "almost home", id: 1 },
+    { title: "memory gospel", id: 2 },
+    { title: "this wild darkness", id: 3 },
+  ]);
+  const addSong = title => {
+    setSongs([...songs, { title, id: uuid() }]);
+  };
+  useEffect(() => {}, []);
+  return (
+    <div className="song-list">
+      <ul>
+        {songs.map(song => (
+          <li key={song.id}>{song.title}</li>
+        ))}
+      </ul>
+      <NewSongForm addSong={addSong} />
+    </div>
+  );
+};
+
+export default SongList;
